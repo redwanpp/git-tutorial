e533e327c6e473e22735da166ebe012a7ef43768
redwanpp <smredwan.info@gmail.com>
2024-03-01 09:33:22 +0600

    added new method called greeting to Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,11 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("VSCode is awesome!!!");
+
+        greeting("Redwan");
+    }
+
+    public static void greeting (String name) {
+        System.out.println("Hello " + name);
     }
 }
